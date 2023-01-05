5482d7d2a45861978615002558e10000980e00ee
María Rodríguez <lu.rzapien.92@gmail.com>
2023-01-05 12:41:37 -0600

    Updates

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
-// estas funciones son de ejemplo
-// export const example = () => {
-//   return 'example';
-// };
 
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
+// estas funciones son de ejemplo
+export const example = () => {
+  return 'example';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
+};
 
 export const getBooks = (data) => {
   return data.books;
@@ -16,12 +17,21 @@ export const getCharacters = (data) => {
 }
 
 export const searchCharacterByName = (name, characters) => {
-  if (name === '') return []
+  if (name === '') return [];
 
   const charactersFound = characters.filter(function (character) {
     const characterName = character.name.toLowerCase();
     return characterName.includes(name.toLowerCase());
-  })
+  });
 
   return charactersFound;
+}
+
+export const getSpells = (data) => {
+  return data.spells;
+}
+
+export const searchSpellsByLetter = (letter, spells) => {
+  const filteredSpells = spells.filter((spell) => spell.name.toLowerCase().startsWith(letter));
+  return filteredSpells;
 }
